55583b47de1fcb08373ecc2706adc55859b93b3e
ben <benjaminfkile@gmail.com>
2020-11-06 05:35:48 -0700

    change sq to dq (double quote)

--- src/Row.java
+++ src/Row.java
@@ -3,23 +3,24 @@ import java.util.UUID;
 
 public class Row {
 
-	int numRows = 2000;
+	int numRows = 3000;
 	
 	Random r = new Random();
 	
-	String columns = "\"lat\",\"lng\",\"url\",\"id\",\"user\",\"del\",\"upvotes\",\"trips\",\"uploaded\",\"on\"\n";
+	String columns = "\"lat\",\"lng\",\"url\",\"id\",\"user\",\"del\",\"upvotes\",\"trips\",\"uploaded\",\"on\",\"zone\"\n";
 	
-	char sq='"';
-	String lat = sq + String.valueOf(44.36 + (48.99 - (44.36)) * r.nextDouble()) + sq;
-	String lng = sq +String.valueOf(-116.0458 + (-104.0186 - (-116.0458)) * r.nextDouble()) + sq;
-	String url = sq +"https://i.ibb.co/9G1K3v5/1fd5e3ab-dfc5-4b16-9fa9-c1c2ada4e54b.jpg" + sq;
-	String id = sq +UUID.randomUUID().toString().replace("-", "") + sq;
-	String user = sq +UUID.randomUUID().toString().replace("-", "") + sq;
-	String del = sq +"https://ibb.co/1bPyJnW/54ba619dfb17a18d318c663c31e41b7b" + sq;
-	String upvotes = sq + "{}" + sq;
-	String trips = sq +"{}" + sq;
-	String uploaded =  sq + "1603682935469" + sq; 
-	String on = sq +"t" +sq ;
+	char dq='"';
+	String lat = dq + String.valueOf(33 + (49 - (33)) * r.nextDouble()) + dq;
+	String lng = dq +String.valueOf(-124 + (-71 - (-124)) * r.nextDouble()) + dq;
+	String url = dq +"https://i.ibb.co/9G1K3v5/1fd5e3ab-dfc5-4b16-9fa9-c1c2ada4e54b.jpg" + dq;
+	String id = dq +UUID.randomUUID().toString().replace("-", "") + dq;
+	String user = dq +UUID.randomUUID().toString().replace("-", "") + dq;
+	String del = dq +"https://ibb.co/1bPyJnW/54ba619dfb17a18d318c663c31e41b7b" + dq;
+	String upvotes = dq + "{}" + dq;
+	String trips = dq +"{}" + dq;
+	String uploaded =  dq + "1603682935469" + dq; 
+	String on = dq +"t" +dq ;
+	String zone = dq + String.valueOf(1 + (int)(Math.random() * ((70000 - 1) + 1))) + dq;
 		
 	String newRow = 
 			lat 
@@ -41,5 +42,7 @@ public class Row {
 			+ uploaded 
 			+ "," 
 			+ on
+			+ ","
+			+ zone
 			+"\n";
 }
